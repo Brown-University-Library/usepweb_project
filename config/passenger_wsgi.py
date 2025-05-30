bf897777ec97cc481a099cb29de89b8319dfada1
Nicole Currens <ncurrens@dlibwwwcit.services.brown.edu>
2025-05-30 14:12:20 -0400

    working?

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -1,53 +1,26 @@
-"""
-WSGI config for django_template_project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
-"""
-Prepares application environment.
-Variables assume project setup like:
-stuff
-    django_template_project
-        usep_app
-        config
-    env_usepweb
-"""
-
 import os, pprint, sys
+import shellvars
+from django.core.wsgi import get_wsgi_application
 
 
-## become self-aware, padawan
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# print( 'the initial env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
-## vars
-ACTIVATE_FILE = os.path.abspath( u'%s/../../env_usepweb/bin/activate_this.py' % current_directory )
-PROJECT_DIR = os.path.abspath( u'%s/../../usepweb_project' % current_directory )
-PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
-SETTINGS_MODULE = u'config.settings'
-SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_usepweb/lib/python2.7/site-packages' % current_directory )
+PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+ENV_SETTINGS_FILE = '/opt/local/project_local_settings/usep_django_web_env_settings/usep_django_web_env_settings.sh'
+# source $USEPWEB__SETTINGS  # set in `httpd/passenger.conf`, and `env/bin/activate`
 
-## virtualenv
-execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
+## update path
+sys.path.append( PROJECT_DIR_PATH )
 
-## sys.path additions
-for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
- if entry not in sys.path:
-   sys.path.append( entry )
-
-## environment additions
-os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
+## reference django settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
 
 ## load up env vars
-SETTINGS_FILE = os.environ['USEPWEB__SETTINGS_PATH']  # set in activate_this.py, and activated above
-import shellvars
-var_dct = shellvars.get_vars( SETTINGS_FILE )
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE )
 for ( key, val ) in var_dct.items():
-    os.environ[key] = val
+   os.environ[key.decode('utf-8')] = val.decode('utf-8')
+
+# print( 'the final env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
 ## gogogo
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
